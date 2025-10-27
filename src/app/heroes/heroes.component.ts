617f06e1d6823a85a39dc68420fee0764008ca27
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-27 16:42:37 -0300

    Adiciona funcionalidade de seleção de heróis e exibe detalhes do herói selecionado

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { NgFor, UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
@@ -19,4 +19,12 @@ export class HeroesComponent {
   };
 
   heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  }
+
 }
+
+
