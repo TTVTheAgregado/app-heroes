de263ca4859ffa7a558bc64c9d119295fbd83b19
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-27 15:56:00 -0300

    Adiciona lista de heróis e atualiza o componente Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
   hero: Hero = { 
     id: 1, 
     name: 'Windstorm' 
   };
-}
 
+  heroes = HEROES;
+}
