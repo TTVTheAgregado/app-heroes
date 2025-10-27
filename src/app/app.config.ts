74efaf1cbdd029040bc4f7f5fe7191017da002d3
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-27 15:32:43 -0300

    Adiciona configuração de módulos e declarações para o componente Heroes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
 };
+
+imports : [
+  FormsModule, 
+  BrowserModule
+]
+
+declarations: [
+  HeroesComponent,
+  AppComponent
+]
