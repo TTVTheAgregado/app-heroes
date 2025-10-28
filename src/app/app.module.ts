669f41726d30fa13086732f21a328ee21160d0e0
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-28 16:36:30 -0300

    Criado app.module

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutesModule } from './app.routes.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutesModule
+  ]
+})
+export class AppModule { }
