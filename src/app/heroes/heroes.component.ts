8a2b536e00752bc6c02d4485cc0043071922e2e9
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-28 15:10:56 -0300

    Criado Hero Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
@@ -19,11 +19,23 @@ export class HeroesComponent {
     name: 'Windstorm' 
   };
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+  this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
