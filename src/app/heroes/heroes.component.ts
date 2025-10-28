4189bf5f20c2abd3e491a15430deb08066fdb7c4
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-28 15:52:48 -0300

    Reformulado e em funcionamento MessageHero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -23,21 +24,23 @@ export class HeroesComponent {
 
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: herói selecionado id=${hero.id}`);
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   getHeroes(): void {
   this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
+  
 }
 
 
