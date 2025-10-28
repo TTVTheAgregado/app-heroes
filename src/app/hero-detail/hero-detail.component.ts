879f30168103d2802fee758530c9427be17a2326
TTVTheAgregado <igorcauan036@gmail.com>
2025-10-28 11:22:05 -0300

    Atualizado Hero Details Components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { Component, NgModule, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, FormsModule, UpperCasePipe],
   templateUrl: './hero-detail.component.html',
-  styleUrl: './hero-detail.component.css'
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-
+  @Input() hero!: Hero;
+  
 }
